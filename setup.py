e1a449d5f3a075d32f28f394c040b1c8673af4c3
Roberto Agostino Vitillo <ra.vitillo@gmail.com>
2016-02-11 23:59:18 +0000

    First commit.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='parquet2hive',
+      version='0.1.0',
+      author='Roberto Agostino Vitillo',
+      author_email='rvitillo@mozilla.com',
+      description='Hive import statement generator for Parquet datasets',
+      url='https://github.com/vitillo/parquet2hive',
+      data_files=[('share/parquet2hive', ['parquet2hive/parquet-tools.jar'])],
+      scripts=['parquet2hive/parquet2hive'],
+      install_requires=['boto3'])
