25387ecfcf5d314ba81d5d9e8ec2e2844ad381b3
Roberto Agostino Vitillo <ra.vitillo@gmail.com>
2016-02-26 14:33:40 +0000

    Bump version up.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='parquet2hive',
-      version='0.1.3',
+      version='0.1.4',
       author='Roberto Agostino Vitillo',
       author_email='rvitillo@mozilla.com',
       description='Hive import statement generator for Parquet datasets',
