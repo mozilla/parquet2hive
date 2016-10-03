97a2dbf6341eb9acf0b51ad67576b738970e953c
Frank Bertsch <fbertsch@mozilla.com>
2016-10-03 08:15:53 -0500

    Increase version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='parquet2hive',
-      version='0.2.15',
+      version='0.2.16',
       author='Roberto Agostino Vitillo',
       author_email='rvitillo@mozilla.com',
       description='Hive import statement generator for Parquet datasets',
