8255bb9c98c84cad31460751230cfcff3a4e6689
Blake Imsland <blake@retroco.de>
2017-03-13 19:39:04 -0700

    Use scm version in setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from setuptools import setup
 
-setup(name='parquet2hive',
-      version='0.2.19',
-      author='Roberto Agostino Vitillo',
-      author_email='rvitillo@mozilla.com',
-      description='Hive import statement generator for Parquet datasets',
-      url='https://github.com/mozilla/parquet2hive',
-      scripts=['parquet2hive'],
-      packages=['parquet2hive_modules', 'parquet2hive_modules.parquet_format'],
-      install_requires=['boto3', 'functools32', 'thrift==0.10.0', 'boto>=2.36.0'],
-      setup_requires=['pytest-runner'],
-      tests_require=['pytest', 'moto', 'wheel[signatures]', 'jsonschema'])
+setup(
+    name='parquet2hive',
+    use_scm_version=True,
+    author='Roberto Agostino Vitillo',
+    author_email='rvitillo@mozilla.com',
+    description='Hive import statement generator for Parquet datasets',
+    url='https://github.com/mozilla/parquet2hive',
+    scripts=['parquet2hive'],
+    packages=['parquet2hive_modules', 'parquet2hive_modules.parquet_format'],
+    install_requires=['boto3', 'functools32',
+                      'thrift==0.10.0', 'boto>=2.36.0'],
+    setup_requires=['pytest-runner', 'setuptools_scm'],
+    tests_require=['pytest', 'moto', 'wheel[signatures]']
+)
